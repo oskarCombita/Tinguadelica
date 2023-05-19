5d41246f7c16cdb897ee1649ad28b8d2ebce0e29
JuanSalguero5 <jrnmslgr@gmail.com>
2023-05-19 12:32:19 -0500

    Correción_
    Correcion ScriptSnakeAttack_

--- Assets/Scripts/SnakeScripts/SnakeAttack.cs
+++ Assets/Scripts/SnakeScripts/SnakeAttack.cs
@@ -21,9 +21,22 @@ public class SnakeAttack : MonoBehaviour
             birdController.live--;
             uiManager.LoseLife();
             //Debug.Log("El jugador ha sido golpeado por Snake");
-            StartCoroutine(birdController.BlinkColor());
+            //StartCoroutine(birdController.BlinkColor());
+            birdController.StartBlinkColor();
             birdController.ShowVFXDamage();
             Invoke("ShowVFXDamage", 0.1f);
+        }
+        if (uiManager.countMushrooms >= 1)
+        {
+            uiManager.countMushrooms--;
+            uiManager.UpdateMushroomUiCount();
+            birdController.ShowVFXLoseMush();
+        }
+        else if (uiManager.countMushrooms >= 2)
+        {
+            uiManager.countMushrooms -=2;
+            uiManager.UpdateMushroomUiCount();
+            birdController.ShowVFXLoseMush();
         }
     }
 }
