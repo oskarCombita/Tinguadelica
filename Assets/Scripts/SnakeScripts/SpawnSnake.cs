3772074d0047a8172774cb69bd9fee84f6e8c6df
Oskar Quiroga <oskar.quiroga@gmail.com>
2023-05-19 18:56:00 -0500

    Clean scripts + Spawn Snake
    Spawn snake is invoked on spawnbads

--- Assets/Scripts/SnakeScripts/SpawnSnake.cs
+++ Assets/Scripts/SnakeScripts/SpawnSnake.cs
@@ -6,27 +6,11 @@ using UnityEngine.UIElements;
 public class SpawnSnake : MonoBehaviour
 {
     [SerializeField]private GameObject snake;
-    //private BirdController birdController;
-    private UiManager uiManager;
 
-    void Start()
-    {
-        uiManager = GameObject.Find("Lives UI").GetComponent<UiManager>();
-        InstantiateSnake();
-       
-    }
-
-    private void Update()
-    {
-       
-    }
 
     public void InstantiateSnake()
     {
 
-        ///Instantiate(snake, new Vector2(5, 2), Quaternion.identity);
-
-        //if (uiManager.countMushrooms > 1)
 
             GameObject snakeInstance = Instantiate(snake, new Vector2(5, 2), Quaternion.identity);
 
