5522678b9961f4633261b3a02ffd033366c61acb
Oskar Quiroga <oskar.quiroga@gmail.com>
2023-05-26 20:35:17 -0500

    Upgrate Animations
    Game Over and Level Complete

--- Assets/Scripts/SnakeScripts/SnakeAttack.cs
+++ Assets/Scripts/SnakeScripts/SnakeAttack.cs
@@ -9,7 +9,7 @@ public class SnakeAttack : MonoBehaviour
     private GameManager gameManager;
 
     private AudioSource snakeAudioSource;
-    public AudioClip soundLossLife;
+    public AudioClip soundLossLife; 
     public AudioClip soundLossmushroom;
 
     private void Awake()
@@ -20,7 +20,7 @@ public class SnakeAttack : MonoBehaviour
     private void Start()
     {
         uiManager = GameObject.Find("Lives UI").GetComponent<UiManager>();
-        gameManager = GetComponent<GameManager>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
