85f5590091ef427e67efab96ceab67a79552ff49
Oskar Quiroga <oskar.quiroga@gmail.com>
2023-07-17 20:41:48 -0500

    Upadted mobile controls
    Add mobile control scheme

--- Assets/Scripts/ShowOnMobile.cs
+++ Assets/Scripts/ShowOnMobile.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class ShowOnMobile : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         gameObject.SetActive(Application.isMobilePlatform);
